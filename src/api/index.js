b550746c623d439049e47dc8f9d27fc6ac5b9d1d
Soravis Prakkamakul <asunnotthesun@gmail.com>
2017-04-23 07:39:02 +0700

    Added twilio response

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import { version } from '../../package.json';
 import { Router } from 'express';
 import facets from './facets';
+var twilio = require('twilio');
 
 export default ({ config, db }) => {
 	let api = Router();
@@ -16,7 +17,12 @@ export default ({ config, db }) => {
   // perhaps expose some API metadata at the root
   api.post('/checkup', (req, res) => {
     console.log(req.body);
-    res.json({ message:'checkup ja' });
+    //res.json({ message:'checkup ja' });
+    let twiml = new twilio.TwimlResponse();
+    twiml.say('Your daily checkup is now completed. Thank you', { voice: 'alice' });
+    // Render the response as XML in reply to the webhook request
+    res.type('text/xml');
+    res.send(twiml.toString());
   });
 
 	// perhaps expose some API metadata at the root
